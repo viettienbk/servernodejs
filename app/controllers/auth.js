bc3dbe660714ee9d1d85c6ec3f4bb1e84541d38f
viettienbk <viettienbk@gmail.com>
2017-02-28 23:51:40 +0700

    update login

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -3,27 +3,29 @@
 var User = require('../models/user');
 
 module.exports = function(app, passport) {
-    app.post('/login', passport.authenticate('local-login', {
-        failureFlash: 'login failed'
-    }), function(req, res) {
-        //  login success
-        res.json({ message: 'login success' });
+
+    app.post('/login', function(req, res, next) {
+        passport.authenticate('local-login', function(err, user, info) {
+            console.log(err);
+            console.log(user);
+            console.log(info);
+            if (err) {
+                res.json({ error: 'login error' });
+            } else if (!user) {
+                res.json({ error: req.flash('message') });
+            } else {
+                res.json({ user: user });
+            }
+        })(req, res, next);
     });
-    /*
-    app.post('/signup', passport.authenticate('local-signup', {
-        failureFlash: 'signup failed'
-    }), function(req, res) {
-        //  signup success
-        res.json({ message: 'signup success' });
-    });
-    */
+
     app.post('/signup', function(req, res, next) {
         passport.authenticate('local-signup', function(err, user, info) {
             console.log(err);
             console.log(user);
             console.log(info);
             if (err) {
-                res.json({ message: 'sign up error' });
+                res.json({ error: 'sign up error' });
             } else if (!user) {
                 res.json({ error: req.flash('message') });
             } else {
