251568d04d6b36aab8cc82d432b46fea2a439156
viettienbk <viettienbk@gmail.com>
2017-05-03 18:32:38 +0700

    update api

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //  database path
-    durl: 'mongodb://viettienbk:12345678@ds058369.mlab.com:58369/biker'
+    durl: 'mongodb://viettienbk:123456@ds129651.mlab.com:29651/biker'
         //  durl: 'mongodb://localhost:27017/biker'
 }
